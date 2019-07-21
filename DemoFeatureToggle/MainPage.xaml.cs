b035e0c5ca996773e602591477aceadcb4b77823
Juliano Custodio <juliano.custodio@hotmail.com.br>
2019-07-21 19:00:02 -0300

    Implement Feature Toggle using ConfigCat SDK

--- DemoFeatureToggle/MainPage.xaml.cs
+++ DemoFeatureToggle/MainPage.xaml.cs
@@ -1,21 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using ConfigCat.Client;
 using Xamarin.Forms;
 
 namespace DemoFeatureToggle
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
+
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        /*AutoPollConfiguration clientConfiguration = new AutoPollConfiguration
+        {
+            ApiKey = "API KEY",
+            PollIntervalSeconds = 15
+        };
+        */
+        /*
+        ManualPollConfiguration clientConfiguration = new ManualPollConfiguration
+        {
+            ApiKey = "API KEY"
+        };*/
+
+        LazyLoadConfiguration clientConfiguration = new LazyLoadConfiguration
+        {
+          ApiKey = "API KEY",
+          CacheTimeToLiveSeconds = 240
+        };
+
+        IConfigCatClient client;
+
         public MainPage()
         {
             InitializeComponent();
+            client = new ConfigCatClient(clientConfiguration);
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            //client.ForceRefresh();
+            if (client.GetValue("isAwesomeFeatureEnabled", false))
+            {
+                LblResult.Text = "ON";
+                LblResult.TextColor = Color.GreenYellow;
+            }
+            else
+            {
+                LblResult.Text = "OFF";
+                LblResult.TextColor = Color.Red;
+            }
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            client.Dispose();
         }
     }
 }
+
+
